fe0d8da109035dbf467c5a97325e7cc1100f3e66
zhangweibin <zhangweibin@192.168.2.115>
2017-05-26 14:12:54 +0800

    12

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -1,11 +1,23 @@
 package com.project;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class ProjectApplication {
 
+	
+	@Bean(name = "jdbcTemplate")
+	public JdbcTemplate primaryJdbcTemplate(
+	        @Qualifier("dataSource") DataSource dataSource) {
+	    return new JdbcTemplate(dataSource);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
--- src/test/java/com/project/ProjectApplicationTests.java
+++ src/test/java/com/project/ProjectApplicationTests.java
@@ -2,15 +2,33 @@ package com.project;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.project.repository.TestPepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjectApplicationTests {
 
+	@Autowired
+	@Qualifier("jdbcTemplate")
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private TestPepository testPepository;
+	
 	@Test
-	public void contextLoads() {
+	public void TestJdbcTemplate() {
+		System.out.println(jdbcTemplate);
+	}
+	
+	@Test
+	public void TestPepository() {
+		System.out.println(testPepository);
 	}
 
 }
